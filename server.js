194b9c73dbf0fa928fb57c8412e29839651aa6ef
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-15 17:54:52 -0700

    using vue + express + socket is complicated

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-// get my NPM stuff
+// require my NPM stuff
 let Express = require('express')
-require('dotenv').config()
-
-// initialize for use
 let app = Express()
 let server = require('http').createServer(app)
+let Vue = require('vue/dist/vue.js')
+let SocketIO = require('socket.io')
+require('dotenv').config()
 
+// vue components
+// Vue.component('navbar', require('./views/components/Navbar.vue'))
+
+// initialize for use
+// app.set('view engine', 'vuexpress')
+// app.use('/npm', Express.static('node_modules'))
+// app.use(Express.static('app'))
+
+// some basic routes
 app.get('/', (req, res) => {
-    res.send(`🦉 what a hoot 🦉`)
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 server.listen(process.env.PORT || 5000, () => {
