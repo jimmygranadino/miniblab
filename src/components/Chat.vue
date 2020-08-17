11f4752b37423cba838384f53518f879ebaa2242
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-17 11:59:59 -0700

    lunch progress

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="row mx-auto">
         <div class="col blab-bg">
-            <div class="row mx-5 my-5 px-3 py-3 chat-bg"><h1>test</h1></div>
+            <div class="row mx-5 my-5 px-3 py-3 chat-bg">
+                    <ul id="messages"></ul>
+                    <form action="">
+                        <input id="m" autocomplete="off" /><button>Send</button>
+                    </form>
+            </div>
         </div>
     </div>
 </template>
@@ -13,5 +18,12 @@ export default {
 </script>
 
 <style scoped>
-
+    * { margin: 0; padding: 0; box-sizing: border-box; }
+        body { font: 13px Helvetica, Arial; }
+        form { background: #000; padding: 3px; position: absolute; bottom: 0; width: 100%; }
+        form input { border: 0; padding: 10px; width: 90%; margin-right: 0.5%; }
+        form button { width: 9%; background: rgb(130, 224, 255); border: none; padding: 10px; }
+        #messages { list-style-type: none; margin: 0; padding: 0; }
+        #messages li { padding: 5px 10px; }
+        #messages li:nth-child(odd) { background: #eee; }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="row justify-content-end mx-auto footer-bg">
         <div class="col-4">
-            <h1 class="text-right">Test</h1>
-        </div>
-        <div class="col-4">
-            <h1 class="text-right">Test</h1>
+            <h6 class="pt-3">Licensing:</h6>
+            <p class=""><small>All images sourced from Unsplash</small></p>
+            <p class=""><small>est. 2020</small></p>
         </div>
     </div>
 </template>
