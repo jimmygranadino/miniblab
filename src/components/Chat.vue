871ae3ff4f468f879cd22024fe6bfeca537e84d9
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-17 10:06:29 -0700

    chat component build out

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-
+    <div class="row mx-auto">
+        <div class="col blab-bg">
+            <div class="row mx-5 my-5 px-3 py-3 chat-bg"><h1>test</h1></div>
+        </div>
     </div>
 </template>
 
