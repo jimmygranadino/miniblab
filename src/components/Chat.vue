9a2f2c2bf3d816fe4f864a1d8c69015bc0347a27
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-19 10:34:09 -0700

    netlify deploy

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,7 +2,7 @@
     <div class="row justify-content-center mx-auto" id="display">
         <div class="col-lg-10 mt-5 rounded blab-bg">
             <div class="row mx-5 my-5 px-3 py-3 chat-bg">
-                <iframe class="col" style="border:0;" src="http://localhost:5000/" allowfullscreen></iframe>
+                <iframe class="col" style="border:0;" src="https://vuesocketblab.herokuapp.com/" allowfullscreen></iframe>
             </div>
         </div>
     </div>
